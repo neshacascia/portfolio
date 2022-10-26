02b1e02e4f495c074be9cc9ca75848241e2dcf82
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-26 15:33:10 -0400

    mobile hamburger menu

--- main.js
+++ main.js
@@ -1,42 +1,8 @@
-// Creations Carousel:
-const slides = document.querySelectorAll('.carousel-item')
-let slidePosition = 0
-const totalSlides = slides.length
+// Mobile Hamburger Menu:
+const hamburgerMenu = document.querySelector(".nav-mobile");
 
-const nextButton = document.querySelector('.carousel-button-next')
-const prevButton = document.querySelector('.carousel-button-prev')
+hamburgerMenu.addEventListener("click", displayNavMenu);
 
-nextButton.addEventListener("click", moveNextSlide)
-prevButton.addEventListener('click', movePrevSlide)
-
-
-function hideAllSlides() {
-    for (let slide of slides) {
-        slide.classList.remove('carousel-item-current')
-        slide.classList.add('carousel-item')
-    }
-}
-
-function moveNextSlide() {
-    hideAllSlides()
-    
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0
-    } else {
-        slidePosition++
-    }
-
-    slides[slidePosition].classList.add('carousel-item-current')
-}
-
-function movePrevSlide() {
-    hideAllSlides()
-
-    if (slidePosition === 0) {
-        slidePosition = totalSlides - 1
-    } else {
-        slidePosition--
-    }
-
-    slides[slidePosition].classList.add('carousel-item-current')
+function displayNavMenu() {
+  console.log("I am being clicked!");
 }
