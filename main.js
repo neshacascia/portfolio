c85c1c78611aac582781d1840f024460831ff7a1
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-27 17:48:53 -0400

    starting mobile hamburger menu

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// Mobile Hamburger Menu:
+//Mobile Hamburger Menu:
 const hamburgerMenu = document.querySelector(".nav-mobile");
+const mobileNav = document.querySelector(".hamburger-menu");
 
 hamburgerMenu.addEventListener("click", displayNavMenu);
 
 function displayNavMenu() {
   console.log("I am being clicked!");
+  mobileNav.style.display = "block";
 }
