5fb350eec5535e076973d87824a4d84b5a46345e
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-28 12:07:51 -0400

    cleaned up usused code

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 const hamburgerMenu = document.querySelector(".nav-mobile");
 const mobileNav = document.querySelector(".hamburger-menu");
 
+let mobileNavToggle = true;
+
 hamburgerMenu.addEventListener("click", displayNavMenu);
 
 function displayNavMenu() {
   console.log("I am being clicked!");
-  mobileNav.style.display = "block";
+  mobileNav.style.display === "block";
 }
