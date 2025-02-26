cddccfd4ac8ccd4156fef3f3d832a664e88a1153
Nesha Mervin <nesha.mervin@gmail.com>
2025-02-25 19:39:54 -0500

    added contact me section to bottom of case study with buttonto copy email address

--- main.js
+++ main.js
@@ -32,31 +32,17 @@ function toggleTheme() {
   }
 }
 
-// form submission:
-document
-  .querySelector('#contact-form')
-  .addEventListener('submit', handleSubmit);
+// copy email:
+function copyEmail() {
+  const email = 'neshacascia@gmail.com';
+  navigator.clipboard.writeText(email).then(() => {
+    const messageModal = document.querySelector('#copy-message');
+    messageModal.classList.remove('hidden');
+    messageModal.classList.add('flex');
+    setTimeout(() => {
+      messageModal.classList.add('hidden');
+    }, 2500);
 
-function handleSubmit(e) {
-  e.preventDefault();
-
-  const myForm = e.target;
-  const contactFormData = new FormData(myForm);
-
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams(contactFormData).toString(),
-  })
-    .then(() => {
-      const messageModal = document.querySelector('#message-modal');
-      messageModal.classList.remove('hidden');
-      messageModal.classList.add('flex');
-      setTimeout(() => {
-        messageModal.classList.add('hidden');
-      }, 2500);
-      myForm.reset();
-      console.log('Form successfully submitted');
-    })
-    .catch(error => alert(error));
+    console.log('Email copied to clipboard!');
+  });
 }
